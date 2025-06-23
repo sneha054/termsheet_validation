71334920999d1c9911c33464d269e67f776ad289
Pavankumar-Batchu1185 <166200340+Pavankumar-Batchu1185@users.noreply.github.com>
2025-06-23 14:26:56 +0530

    This script is used for convert text file data to pdf json

--- zip_extraction/zipfiledatatojson.py
+++ zip_extraction/zipfiledatatojson.py
@@ -12,20 +12,14 @@ def extract_cleaned_data_from_zip_file(filepath):
     return {"sections":sections,"exhibit_a":exhibit} 
 
 def parse_sections(text)->dict:
-    
-    #we have to detect headings first before exhibit A
-    #exhibit A is not the main data it is extra information for our data
-
-    #document has basically 1]headings and paragraphs 2]field value
-    #we have to build dictionary
 
     data={}
-    current_section=None #current section
+    current_section=None 
 
-    data_under_section=[] #to collect lines under current section
+    data_under_section=[] 
 
     for line in text:
-        line=line.strip() #we use strip() to remove whitespace/newlines
+        line=line.strip() 
 
         if line =="*EXHIBIT A*":
             break
@@ -33,25 +27,24 @@ def parse_sections(text)->dict:
             
            if current_section:
               data[current_section]="".join(data_under_section).strip().strip("*")
-           #check if it's a heading
-           #remove * ,splits at : converts to lowercaseand replace spaces with underscore
+
+            
            key=line.strip("* ").split(":",1)[0].strip().lower().replace(" ","_")
 
            current_section=key
 
-           data_under_section=[line.split(":",1)   [1].strip()] #text after colon
+           data_under_section=[line.split(":",1)   [1].strip()] 
 
         elif line.startswith("*") and not ":"in line:
 
            if current_section:
               data[current_section]="".join(data_under_section).strip()
-           #check if it's a heading
-           #remove * ,splits at : converts to lowercaseand replace spaces with underscore
+           
            key=line.strip("* ").strip().lower().replace(" ","_")
 
            current_section=key
 
-           data_under_section=[] #text after colon
+           data_under_section=[] 
         
         else:
             data_under_section.append(line)
@@ -90,7 +83,6 @@ def parse_rows_of_the_table(lines):
 
     i=0
 
-    #read headers(lines starting with *)
     while i< len(lines):
         
         line  =lines[i].strip()
@@ -105,10 +97,8 @@ def parse_rows_of_the_table(lines):
         else:
             break
 
-    #parse data in chunks matching header length
-
     def is_number(value):
-        return re.match(r"^-?\d*\.?\d+$",value) #^-start of string ,- for negative numbers \d:one or more digits ,then decimal points $:end of string
+        return re.match(r"^-?\d*\.?\d+$",value) 
 
 
     while i + len(headers)<=len(lines):
@@ -142,8 +132,6 @@ if __name__=="__main__":
 
     with open("structureddatafortextfile.json","w",encoding="utf-8") as f:
         json.dump(output,f,indent=2)
-
-#deafult windows encoding-cp1252    
 
 
             
