3aaa7cf1333f7d6916134e6339c2163afcb5ef67
Pavankumar-Batchu1185 <166200340+Pavankumar-Batchu1185@users.noreply.github.com>
2025-07-12 23:30:21 +0530

    Update worddocextractor.py

--- docx_extraction/worddocextractor.py
+++ docx_extraction/worddocextractor.py
@@ -1,4 +1,5 @@
 from docx import Document
+import sys
 
 def extract_text_from_worddocx(docx_path):
 
@@ -21,9 +22,20 @@ def extract_tables_from_worddocx(docx_path):
             textundertables.append('\t'.join(row_data))
     return '\n'.join(textundertables)
     
+def save_text_to_file(text:str,filename,mode):
+    with open(filename,mode,encoding="utf-8") as f:
+        f.write(text)
 
-docx_path='CFI-Term-Sheet-Template.docx'
-text=extract_text_from_worddocx(docx_path)
-text1=extract_tables_from_worddocx(docx_path)
-print(text)
-print(text1)
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print("Running word_extractor.py")
+        sys.exit(1)
+
+    docx_path=sys.argv[1]
+    text=extract_text_from_worddocx(docx_path)
+    text1=extract_tables_from_worddocx(docx_path)
+    print(text)
+    print(text1)
+    output_path="D:\\termsheet_validation\\docx_extraction\\text_output2.txt"
+    save_text_to_file(text,output_path,'w')
+    save_text_to_file(text1,output_path,'a')
